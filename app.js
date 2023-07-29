1b31a7eb747e5b8402c71e67d444c0f026d9636d
K1kva2006 <115255875+K1kva2006@users.noreply.github.com>
2023-07-29 18:08:24 +0400

    1.5

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const button = document.getElementById("button");
 
-const text = document.getElementById("text")
+const text = document.getElementById("text");
+
+
 
 button.addEventListener("click", () => {
     if (button.classList.toggle("activeButton")) {
-        button.textContent = "Light mode"
-        document.body.style.backgroundColor = "black"
-        document.body.style.transition = "1s"
-        text.classList.toggle("text-active")
-        text.textContent = "Dark mode"
+        localStorage.displayMode = 1
+        button.textContent = "Light mode";
+        document.body.style.backgroundColor = "black";
+        document.body.style.transition = "1s";
+        text.classList.toggle("text-active");
+        text.textContent = "Dark mode";
     } else {
-        button.textContent = 'Dark mode'
-        document.body.style.backgroundColor = "white"
-        text.classList.toggle("text-active")
-        text.textContent = "Light mode"
+        localStorage.displayMode = 0
+        button.textContent = "Dark mode";
+        document.body.style.backgroundColor = "white";
+        text.classList.toggle("text-active");
+        text.textContent = "Light mode";
     }
-})
+});
+
